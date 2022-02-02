195a681cc90b461bf3ea174f2c021831155b631c
Jardesson Ribeiro <jardessonrs1117@gmail.com>
2022-02-02 18:59:16 -0300

    added player check

--- src/scripts/mecanic.js
+++ src/scripts/mecanic.js
@@ -1,22 +1,75 @@
-var init = '';
-var end = '';
-var vez = null;
-var parentButton = null;
+ posicaoInialDaPeca = '';
+var destinoDaPeca = '';
+var vezDeJogar = null;
+var jogoIniciado = false;
+var ultimaPecaSelecionada  = null;
+const jogador1 = "jogador1";
+const jogador2 = "jogador2";
 
-function reset() {
-    init = '';
-    end = '';
+function reset(){
+    posicaoInialDaPeca = '';
+    destinoDaPeca = '';
+    // vezDeJogar = null;
+    // jogoIniciado = false;
+    // ultimaPecaSelecionada  = null;
 }
 
-function select(id) {
-    init = id;
-    const button = document.getElementById(id).parentNode;
-    console.log(button.id);
+function select(idButton) {
+    if(vezDeJogar === null){
+        vezDeJogar = setJogador(idButton);
+        ultimaPecaSelecionada = idButton;
+        posicaoInialDaPeca = document.getElementById(idButton).parentNode.id;
+        return;
+    }
+
+    if(vezDeJogar === setJogador(idButton)){
+        ultimaPecaSelecionada = idButton;
+        posicaoInialDaPeca = document.getElementById(idButton).parentNode.id;
+    }else {
+        alert("Não é sua vez de jogar");
+    }
 }
 
-function finish(id){
-    end = id;
-    const button = document.getElementById('1');
-    const div    = document.getElementById(id);
+function jump(coordinate){
+    const div    = document.getElementById(coordinate);
+    console.log(div.firstElementChild);
+    if(div.firstElementChild){
+        return;
+    }
+    //Dar para trocar por ultima peça selecionada
+    const button = document.getElementById(ultimaPecaSelecionada);
+    if(vezDeJogar !== setJogador(button.id)){
+        return;
+    }
+
+    const [lineStarting, columnStarting ] = posicaoInialDaPeca.split(":");
+    const [lineDestination, columnDestinaion ] = coordinate.split(":");
+    // console.log({lineStarting, columnStarting, lineDestination, columnDestinaion});
     div.appendChild(button);
+    vezDeJogar = alternaVez(button.id);
 }
+
+function setJogador(idButton){
+    if(idButton <= 12 & idButton >=1){
+        return jogador1;
+    }
+
+    if(idButton > 12 & idButton <= 24){
+        return jogador2;
+    }
+}
+
+function alternaVez(idButton){
+    return setJogador(idButton) === jogador1 ? jogador2 : jogador1;
+}
+
+function verifyCoordinate(coordinates, idButton){
+    if(coordinates.lineStarting === coordinates.lineDestination){
+        return false;
+    }
+    
+    if(coordinates.columnStarting === coordinates.columnDestinaion){
+        return false;
+    }
+}
+
